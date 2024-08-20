a96c92285e3eed1980ba5bd9d7081e15bdf2d000
Alex Gisi <apgisi11@gmail.com>
2024-08-20 15:12:44 -0500

    Don't create directory until it's needed
    * Formatting
    * Fix directory name

--- csv_logger/csv_logger.py
+++ csv_logger/csv_logger.py
@@ -11,24 +11,26 @@ class CSVLoader:
 
     def get_dfs(self):
         return {
-            os.path.splitext(os.path.basename(file))[0]: pd.read_csv(file)  # { Module name : Dataframe }
+            os.path.splitext(os.path.basename(file))[0]: pd.read_csv(
+                file
+            )  # { Module name : Dataframe }
             for file in glob.glob(os.path.join(self.path, "*.csv"))
         }
-    
+
     def get_combined_df(self):
         dfs = self.get_dfs()
         combined = dfs[0]
         for df in dfs[1:]:
-            combined = combined.merge(df, on='time')
+            combined = combined.merge(df, on="time")
         return combined
 
 
 class LoggingModule:
     def __init__(self, path: str, keys: List[str]) -> None:
-        self.file = open(path, 'w', newline='')
+        self.file = open(path, "w", newline="")
         self.keys = keys
 
-        self.writer = csv.DictWriter(self.file, fieldnames=keys)
+        self.writer = csv.DictWriter(self.file, fieldnames=["time"] + keys)
         self.writer.writeheader()
 
     def log(self, time, data):
@@ -43,17 +45,18 @@ class LoggingModule:
 
 class CSVLogger:
     def __init__(self, log_dir: str):
-        if not os.path.exists(log_dir):
-            os.mkdir(log_dir)
         self.log_dir = log_dir
         self.modules = {}
 
     def log(self, time, data: Dict[str, Dict]):
+        if not os.path.exists(self.log_dir):
+            os.makedirs(self.log_dir)
+
         for module_name, module_data in data.items():
             if module_name not in self.modules:
                 self.modules[module_name] = LoggingModule(
-                    os.path.join(self.log_dir, module_name, ".csv"), 
-                    list(module_data.keys())
+                    os.path.join(self.log_dir, f"{module_name}.csv"),
+                    list(module_data.keys()),
                 )
 
             self.modules[module_name].log(time, module_data)
